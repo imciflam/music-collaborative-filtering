4b797818c77ff58438da2baed6292528a70bfd0a
Vita <vitalia@myrambler.ru>
2020-01-26 17:48:52 +0300

    + error handling, + jit

--- flask_api_win.py
+++ flask_api_win.py
@@ -1,12 +1,13 @@
 from flask import Flask, json, request
 import pandas as pd
-import numpy as np 
+import numpy as np
 from fuzzywuzzy import fuzz
 import pickle
 from numba import jit
 from random import random
 import threading
 import time
+import sys
 
 app = Flask(__name__)
 
@@ -29,6 +30,7 @@ def background_calculation():
     result = 42
 
 
+@jit(parallel=True)
 def data_processing():
     rus_data = pd.read_csv('short_well.csv')
     wide_artist_data_pivoted = rus_data.pivot(
@@ -38,7 +40,7 @@ def data_processing():
     wide_artist_data_zero_one = wide_artist_data.apply(np.sign)
     return wide_artist_data_zero_one
 
-
+ 
 def print_artist_recommendations(query_artist, artist_plays_matrix, knn_model, k):
     query_index = None
     list_of_closest_groups = []
@@ -48,15 +50,11 @@ def print_artist_recommendations(query_artist, artist_plays_matrix, knn_model, k
         if ratio >= 75:
             current_query_index = artist_plays_matrix.index.tolist().index(i)
             ratio_tuples.append((i, ratio, current_query_index))
-
-    # print('Possible matches: {0}\n'.format(
-    #    [(x[0], x[1]) for x in ratio_tuples]))
-
     try:
         # get the index of the best artist match in the data
         query_index = max(ratio_tuples, key=lambda x: x[1])[2]
     except:
-     #   print('No match. Try again')
+        print('No match. Try again')
         return None
 
     distances, indices = knn_model.kneighbors(
@@ -64,10 +62,6 @@ def print_artist_recommendations(query_artist, artist_plays_matrix, knn_model, k
 
     for i in range(0, len(distances.flatten())):
         if i != 0:
-            # print('{0}: {1}, with distance of {2}:'.format(
-            #    i, artist_plays_matrix.index[indices.flatten()[i]], distances.flatten()[i]))
-            # list1[artist_plays_matrix.index[indices.flatten()[i]]] = distances.flatten()[
-            #    i]
             list_of_closest_groups.append(
                 (artist_plays_matrix.index[indices.flatten()[i]]))
     return list_of_closest_groups
@@ -83,12 +77,16 @@ result = None
 
 def main():
     thread = threading.Thread(target=background_calculation)
-    print('thread start')
+    print('background_calculation start')
     thread.start()
     # wait here for the result to be available before continuing
     thread.join()
     print('The result is', result)
-    print('background_calculation is completed')
+    if result == 42:
+        print('background_calculation is completed')
+    else:
+        print('background_calculation failed')
+        sys.exit(0)
 
 
 if __name__ == '__main__':
